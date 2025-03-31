ca58e4adea50816bbc6ee1e4ac316025ec786c94
Guto Xavier <gutoffline@gmail.com>
2025-03-31 19:13:16 -0300

    refatoração de código

---
+++ app.js
@@ -0,0 +1,17 @@
+let nomeCompleto = ""
+nomeCompleto = prompt("Informe seu nome completo")
+
+let cor = prompt("Informe uma cor: \n 1 azul \n 2 vermelho \n 3 preto")
+let corDeFundo = null
+
+if(cor == "1"){
+    corDeFundo = "blue"
+}else if(cor == "2"){
+    corDeFundo = "red"
+}else if(cor == "3"){
+    corDeFundo = "black"
+}else{
+    corDeFundo = "yellow"
+}
+
+document.body.style.backgroundColor = corDeFundo
